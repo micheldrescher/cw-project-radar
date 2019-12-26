4438101a12245c4706786143a6a63f533c61703e
Michel Drescher <Michel.Drescher@gmail.com>
2019-12-26 20:13:30 +0000

    Catch-all route

--- src/server/app.js
+++ src/server/app.js
@@ -8,4 +8,12 @@ app.use(express.json()) // automatic JSON <--> object processing
 // MOUNT ROUTERS
 //
 
+// FINAL CATCHALL to handle undefined routes
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Cannot find ${req.originalUrl} on this server.`
+    })
+})
+
 export { app as default }
