9584607138c2d08cec1cd29b2bdb40e76ccbbc08
Michel Drescher <Michel.Drescher@gmail.com>
2020-02-14 11:34:15 +0000

    Classification controller not needed, adding standard virtuals support for JSON rendering

--- models/classificationModel.js
+++ models/classificationModel.js
@@ -14,41 +14,47 @@ const segments = process.env.MODEL_SEGMENTS.split(',').map(e => e.trim())
 //
 // SCHEMA
 //
-const classificationSchema = new mongoose.Schema({
-    // the classification of the proect.
-    // Validated against allowed values configured through .env
-    classification: {
-        type: String,
-        required: true,
-        validate: v => segments.includes(v)
-    },
-    // which project was classified?
-    project: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Project',
-        required: ['A classification must belong to a proejct', true]
-    },
-    // track the date of this classification change
-    classifiedOn: {
-        type: Date,
-        required: true,
-        default: Date.now()
-    },
-    // who initiated the (updated) classification
-    classifiedBy: {
-        type: String,
-        required: true,
-        default: 'Cyberwatching',
-        enum: {
-            values: ['Cyberwatching', 'Project']
+const classificationSchema = new mongoose.Schema(
+    {
+        // the classification of the proect.
+        // Validated against allowed values configured through .env
+        classification: {
+            type: String,
+            required: true,
+            validate: v => segments.includes(v)
+        },
+        // which project was classified?
+        project: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Project',
+            required: ['A classification must belong to a proejct', true]
+        },
+        // track the date of this classification change
+        classifiedOn: {
+            type: Date,
+            required: true,
+            default: Date.now()
+        },
+        // who initiated the (updated) classification
+        classifiedBy: {
+            type: String,
+            required: true,
+            default: 'Cyberwatching',
+            enum: {
+                values: ['Cyberwatching', 'Project']
+            }
+        },
+        // what is the reason for this classification (or the update)
+        changeSummary: {
+            type: String,
+            required: true
         }
     },
-    // what is the reason for this classification (or the update)
-    changeSummary: {
-        type: String,
-        required: true
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
     }
-})
+)
 
 const Classification = mongoose.model('Classification', classificationSchema)
 
