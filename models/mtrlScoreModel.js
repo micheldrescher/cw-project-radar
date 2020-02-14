e4c6bf7db9628762d0d3132253c4f5487ff01043
Michel Drescher <Michel.Drescher@gmail.com>
2020-02-14 11:35:05 +0000

    MTRL and Classification remodelling: Refactoring MTRLScore

--- models/mtrlScoreModel.js
+++ models/mtrlScoreModel.js
@@ -4,26 +4,60 @@
 // libraries
 const mongoose = require('mongoose')
 
-const mtrlScoreSchema = new mongoose.Schema({
-    scoringDate: {
-        type: Date,
-        required: true,
-        default: Date.now()
+//
+// SCHEMAS
+//
+// MTRL schema
+const mtrlScoreSchema = new mongoose.Schema(
+    {
+        // which project was classified?
+        project: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Project',
+            required: ['A classification must belong to a proejct', true]
+        },
+        // when was the score added?
+        scoringDate: {
+            type: Date,
+            required: true,
+            default: Date.now()
+        },
+        // what's the MRL?
+        mrl: {
+            type: Number,
+            required: true,
+            min: [0, 'MRL must be between 0 and 9.'],
+            max: [9, 'MRL must be between 0 and 9.']
+        },
+        // what's the TRL?
+        trl: {
+            type: Number,
+            required: true,
+            min: [0, 'MRL must be between 0 and 9.'],
+            max: [9, 'MRL must be between 0 and 9.']
+        }
+        // score - virtual, calculated using virtual get function
     },
-    mrl: {
-        type: Number,
-        required: true,
-        min: [0, 'MRL must be between 0 and 9.'],
-        max: [9, 'MRL must be between 0 and 9.']
-    },
-    trl: {
-        type: Number,
-        required: true,
-        min: [0, 'MRL must be between 0 and 9.'],
-        max: [9, 'MRL must be between 0 and 9.']
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
     }
+)
+
+//
+// VIRTUALS
+//
+mtrlScoreSchema.virtual('score').get(() => {
+    return this.trl * 2 + this.mrl * 7
 })
 
+//
+// MIDDLEWARE
+//
+
+//
+// MODELS
+//
 const MTRLScore = mongoose.model('MTRLScore', mtrlScoreSchema)
 
 //
