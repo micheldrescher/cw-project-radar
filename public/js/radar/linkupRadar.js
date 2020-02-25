6841b8d286d715acab9ba69ebfb58215bb62fafa
Michel Drescher <Michel.Drescher@gmail.com>
2020-02-25 11:33:17 +0000

    Fancy quadrant zooming in/out

--- public/js/radar/linkupRadar.js
+++ public/js/radar/linkupRadar.js
@@ -15,17 +15,15 @@ export { linkupRadar as default }
 // FUNCTIONS
 //
 const linkupRadar = async radarRootDOM => {
-    console.log(radarRootDOM)
-
     // 1) Mouse over quadrants hghlight
-    highlightQuadrants()
+    interactiveQuadtants()
 }
 
-const highlightQuadrants = () => {
+const interactiveQuadtants = () => {
     d3.selectAll('.segment')
         .on('mouseover', mouseOverQuadrant)
         .on('mouseout', mouseOutQuadrant)
-    // .on('click', selectQuadrant.bind({}, quadrant.order, quadrant.startAngle))
+        .on('click', clickQuadrant)
 }
 
 // highlight the selected quadrant,
@@ -47,28 +45,38 @@ const mouseOutQuadrant = (d, i) => {
     // d3.selectAll('.button.full-view').style('opacity', 1)
 }
 
-function mouseoverQuadrant(order) {
-    d3.select('.quadrant-group-' + order).style('opacity', 1)
-    d3.selectAll('.quadrant-group:not(.quadrant-group-' + order + ')').style('opacity', 0.3)
-}
+// click the quadrant:
+// 1. scales up the selected quadrant and hides all other
+// 2. shows the quadrant's table and hides the others
+const clickQuadrant = (d, i, a) => {
+    const zoomed = d3.select(`g.segment.segment-${i}`).classed('zoomed')
+    // if we are in zoom mode, unzoom
+    if (zoomed) {
+        // unzoom segments
+        d3.select(`g.segment.segment-${i}`)
+            .style('transform', undefined)
+            .classed('zoomed', false)
+        d3.selectAll(`.segment:not(.segment-${i})`).style('transform', undefined)
+        // hide the segment table
+        d3.selectAll(`.segment-table.segment-${i}`).style('display', 'none')
+        return
+    }
 
-function mouseoverQuadrant(order) {
-    // highlight the selected segment
-    d3.select('.quadrant-group-' + order).style('opacity', 1)
-    // dim the non-selected segments
-    d3.selectAll('.quadrant-group:not(.quadrant-group-' + order + ')').style('opacity', 0.3)
-    // highlight the segment's button
-    // d3.select('.button.' + order + '.full-view').style('opacity', 1)
-    // dim all other buttons
-    // d3.selectAll('.button.full-view:not(.' + order+')').style('opacity', 0.3)
-}
+    const theta = 360 / a.length
+    const offset = theta / 2
+    const rotateLeft = i * theta + offset > 180
+    let angle = -i * theta - offset
+    if (rotateLeft) angle = 360 + angle
 
-function mouseoutQuadrant(order) {
-    d3.selectAll('.quadrant-group:not(.quadrant-group-' + order + ')').style('opacity', 1)
+    // clicked segment gets rotated, shifted down and scaleed by two
+    d3.select(`g.segment.segment-${i}`)
+        .style('transform', `scale(2) translateY(25%) rotate(${angle}deg)`)
+        .classed('zoomed', true)
+    // hide the other segments
+    d3.selectAll(`.segment:not(.segment-${i})`).style('transform', 'scale(0)')
+
+    // select the corresponding table
+    d3.selectAll(`.segment-table.segment-${i}`).style('display', 'block')
+    // hide the other tables
+    d3.selectAll(`.segment-table:not(.segment-${i})`).style('display', 'none')
 }
-// function mouseoutQuadrant(order) {
-//     // reset all dimming/highlighting on the segment
-//     d3.selectAll('.quadrant-group:not(.quadrant-group-' + order + ')').style('opacity', 1)
-//     // reset all dimming for the buttons
-//     // d3.selectAll('.button.full-view').style('opacity', 1)
-// }
