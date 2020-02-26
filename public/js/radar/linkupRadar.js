18f0096569af6f7482141a886302558423f94c96
Michel Drescher <Michel.Drescher@gmail.com>
2020-02-26 21:24:45 +0000

    Feature: Blips have a tool tip showing id and name

--- public/js/radar/linkupRadar.js
+++ public/js/radar/linkupRadar.js
@@ -16,16 +16,26 @@ export { linkupRadar as default }
 //
 const linkupRadar = async radarRootDOM => {
     // 1) Animate interactive quadrants
-    interactiveQuadtants()
+    interactiveQuadrants()
     // 2) Popover texts for blips
-    // interactiveBlips()
+    interactiveBlips()
 }
 
-const interactiveQuadtants = () => {
+const interactiveQuadrants = () => {
     d3.selectAll('.segment')
         .on('mouseover', mouseOverQuadrant)
         .on('mouseout', mouseOutQuadrant)
         .on('click', clickQuadrant)
+}
+
+const interactiveBlips = () => {
+    // 1) Configure the tooltip
+    const bliptip = configureBliptip()
+
+    d3.selectAll('.blip')
+        .on('mouseover', (d, i, a) => mouseOverBlip(bliptip, i, a))
+        .on('mouseout', (d, i, a) => mouseOutBlip(bliptip, i, a))
+        .on('click', clickBlip)
 }
 
 // highlight the selected quadrant,
@@ -51,6 +61,8 @@ const mouseOutQuadrant = (d, i) => {
 // 1. scales up the selected quadrant and hides all other
 // 2. shows the quadrant's table and hides the others
 const clickQuadrant = (d, i, a) => {
+    console.log('Quadrant click event handling!')
+
     const zoomed = d3.select(`g.segment.segment-${i}`).classed('zoomed')
     // if we are in zoom mode, unzoom
     if (zoomed) {
@@ -85,4 +97,32 @@ const clickQuadrant = (d, i, a) => {
     d3.selectAll(`.segment-table.segment-${i}`).style('display', 'block')
     // hide the other tables
     d3.selectAll(`.segment-table:not(.segment-${i})`).style('display', 'none')
+}
+
+const configureBliptip = () => {
+    const svg = d3.select('#rendering > svg')
+    const bliptip = d3
+        .tip()
+        .attr('id', 'bliptip')
+        .offset([-8, 0])
+        .html(function(d) {
+            return d
+        })
+    svg.call(bliptip)
+    return bliptip
+}
+
+const mouseOverBlip = (tip, i, a) => {
+    tip.show()
+    tip.show(d3.select(`#${a[i].id}`).attr('label'))
+}
+
+const mouseOutBlip = (tip, i, a) => {
+    tip.hide()
+}
+
+const clickBlip = (d, i, a) => {
+    d3.event.stopPropagation() // stop the event bubbling up the hierarchy
+    console.log('Blip click event handling!')
+    // TODO do something useful, i.e. showing the blip's description as a model
 }
