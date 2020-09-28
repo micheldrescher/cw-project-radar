52534f1ba816245bcf34f55c43a58e871eedf14b
Michel Drescher <Michel.Drescher@gmail.com>
2020-09-28 16:08:25 +0100

    First basic version of a project widget

--- src/server/handlers/modelHandler.js
+++ src/server/handlers/modelHandler.js
@@ -6,19 +6,15 @@
 const catchAsync = require('../utils/catchAsync')
 // const logger = require('./../utils/logger')
 const AppError = require('../utils/AppError')
+const modelController = require('../controllers/modelController')
 
 exports.getDimensions = catchAsync(async (req, res, next) => {
     // get the dimensions from process.env
-    const model = {
-        segments: process.env.MODEL_SEGMENTS.split(',').map((e) => e.trim()),
-        rings: process.env.MODEL_RINGS.split(',').map((e) => e.trim()),
-        lcycle: process.env.MODEL_LCYCLE.split(',').map((e) => e.trim()),
-    }
+    const model = modelController.getModel()
     if (!model.segments || !model.rings || !model.lcycle) {
         throw new AppError('No model data found!', 404)
     }
 
-    console.log(model)
     // return project if found
     res.status(200).json({
         status: 'success',
