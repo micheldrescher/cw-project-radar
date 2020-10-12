91c4fece43bdcbc7b43aeb1204e614899a46b9f3
Michel Drescher <Michel.Drescher@gmail.com>
2020-10-12 18:36:46 +0100

    More transitioning to HTML5 web components

--- src/client/js/radar/linkupRadar.js
+++ src/client/js/radar/linkupRadar.js
@@ -5,6 +5,7 @@
 // app modules
 import showAlert from '../util/alert'
 import showProjectData from './projectInfo'
+import { RadarCoordinates } from '../../../common/widgets/radar-location/radar-coords'
 import { SimpleMetric } from '../../../common/widgets/simple-metric/simple-metric'
 import { SDLCPosition } from '../../../common/widgets/sdlc-position/sdlc-position'
 import { MTRLPerformance } from '../../../common/widgets/mtrl-performance/mtrl-performance'
@@ -21,6 +22,7 @@ const linkupRadar = async (radarRootDOM) => {
     // register custom HTML elements for this radar
     customElements.define('simple-metric', SimpleMetric)
     customElements.define('sdlc-position', SDLCPosition)
+    customElements.define('radar-coords', RadarCoordinates)
     customElements.define('mtrl-performance', MTRLPerformance)
 
     // 1) Animate interactive quadrants
@@ -46,19 +48,23 @@ const interactiveQuadrants = () => {
 // dim the others
 const mouseOverQuadrant = (i) => {
     return (e) => {
-        document.querySelectorAll(`.segment-${i}`).forEach((s) => s.style.opacity = 1)
-        document.querySelectorAll(`.segment:not(.segment-${i})`).forEach((s) => s.style.opacity = 0.3)
+        document.querySelectorAll(`.segment-${i}`).forEach((s) => (s.style.opacity = 1))
+        document
+            .querySelectorAll(`.segment:not(.segment-${i})`)
+            .forEach((s) => (s.style.opacity = 0.3))
     }
 }
 
 // highlight all segments again
 const mouseOutQuadrant = (i) => {
     return (e) => {
-        document.querySelectorAll(`.segment:not(.segment-${i})`).forEach((s) => s.style.opacity = 1)
+        document
+            .querySelectorAll(`.segment:not(.segment-${i})`)
+            .forEach((s) => (s.style.opacity = 1))
     }
 }
 
-const clickQuadrant = (i,l) => {
+const clickQuadrant = (i, l) => {
     return (e) => {
         const zoomed = document.querySelector(`.segment.segment-${i}.zoomed`)
         if (zoomed) zoomOut(i, l)
@@ -66,7 +72,7 @@ const clickQuadrant = (i,l) => {
     }
 }
 
-const zoomIn = (i,l) => {
+const zoomIn = (i, l) => {
     // some calculations
     const theta = 360 / l
     const offset = theta / 2
@@ -79,27 +85,33 @@ const zoomIn = (i,l) => {
     seg.style.transform = `scale(2) translateY(25%) rotate(${angle}deg)`
     seg.classList.add('zoomed')
     // text in clicked segment rotates at inverse angle (to level them again)
-    seg.querySelectorAll('.blip text').forEach((t) => t.style.transform = `rotate(${-angle}deg)`)
+    seg.querySelectorAll('.blip text').forEach((t) => (t.style.transform = `rotate(${-angle}deg)`))
 
     // hide all other segments
-    document.querySelectorAll(`.segment:not(.segment-${i})`).forEach((s) => s.style.transform = 'scale(0)')
-    
+    document
+        .querySelectorAll(`.segment:not(.segment-${i})`)
+        .forEach((s) => (s.style.transform = 'scale(0)'))
+
     // show table for clicked segment
-    document.querySelectorAll(`.segment-table.segment-${i}`).forEach((t) => t.style.display = 'block')
+    document
+        .querySelectorAll(`.segment-table.segment-${i}`)
+        .forEach((t) => (t.style.display = 'block'))
     // hide all other tables
-    document.querySelectorAll(`.segment-table:not(.segment-${i})`).forEach((t) => t.style.display = 'none')
+    document
+        .querySelectorAll(`.segment-table:not(.segment-${i})`)
+        .forEach((t) => (t.style.display = 'none'))
 }
 
 const zoomOut = (i) => {
     document.querySelectorAll('.segment').forEach((s) => {
-        s.style.transform = ''   // remove all transformtion styles from the segments
-        s.classList.remove('zoomed')    // remove zoomed class
+        s.style.transform = '' // remove all transformtion styles from the segments
+        s.classList.remove('zoomed') // remove zoomed class
     })
     document.querySelectorAll(`.segment.segment-${i} .blip text`).forEach((t) => {
-        t.style.transform = ''   // unrotate blips and rings
+        t.style.transform = '' // unrotate blips and rings
     })
     document.querySelectorAll('.segment-table').forEach((t) => {
-        t.style.display = 'none'        // hide the segment table
+        t.style.display = 'none' // hide the segment table
     })
 }
 
@@ -126,8 +138,8 @@ const mouseOverBlip = () => {
         const blipBox = e.target.getBoundingClientRect()
         const ttBox = tt.getBoundingClientRect()
         // move tooltip top of blip, horizontally centered
-        tt.style.left = (window.scrollX+blipBox.left)+blipBox.width/2-ttBox.width/2+'px'
-        tt.style.top = (window.scrollY+blipBox.top-ttBox.height-5)+'px'
+        tt.style.left = window.scrollX + blipBox.left + blipBox.width / 2 - ttBox.width / 2 + 'px'
+        tt.style.top = window.scrollY + blipBox.top - ttBox.height - 5 + 'px'
     }
 }
 
